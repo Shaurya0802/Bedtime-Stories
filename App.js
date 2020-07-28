6ad1216455a65f1e0b2e847371387b7d135df379
Shaurya0802 <shauryaakhileshsharma@gmail.com>
2020-07-29 01:51:32 +0530

    Bedtime-Stories

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import ReadStoryScreen from './screens/ReadScreen';
+import WriteStoryScreen from './screens/WriteScreen';
+
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+
+export default class App extends React.Component() {
+  render(){
+    return(
+      <AppContainer/>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const TabNavigator = createBottomTabNavigator({
+  Read : {screen: ReadStoryScreen},
+  Write : {screen: WriteStoryScreen}
 });
+
+const AppContainer = createAppContainer(TabNavigator);
